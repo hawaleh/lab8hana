aa37bee3904d17d809cefa8f028ef549288af078
hawaleh <hawaleh@gmail.com>
2025-10-29 12:40:29 -0600

    failing delete

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -10,4 +10,19 @@ public class CustomListTest {
 // This will fail initially because hasCity() doesn't exist
         assertTrue(list.hasCity(calgary));
     }
+
+
+    //failign test:
+    @test
+    @Test
+    public void testDeleteCity() {
+        CustomList list = new CustomList();
+        City edm = new City("Edmonton", "AB");
+        list.addCity(edm);
+        list.deleteCity(edm);
+        assertFalse(list.hasCity(edm));
+        City cal = new City("Calgary", "AB");
+        assertThrows(IllegalArgumentException.class, () -> list.deleteCity(cal));
+    }
+
 }
